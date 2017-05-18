54cfeef280c0adb53d0919299754a129c490bd03
Norah Borus <nborus@stanford.edu>
2017-05-17 18:30:45 -0700

    Update play-button.js

--- play-button.js
+++ play-button.js
@@ -3,8 +3,35 @@
 //
 // See HW4 writeup for more hints and details.
 class PlayButton {
-  constructor() {
-    // TODO(you): Implement the constructor and add fields as necessary.
+  constructor(playImage, playContainer, playAudioCallBack, pauseAudioCallBack) {
+    this.MATCH_LIST = {
+      'images/pause.png':'images/play.png',
+      'images/play.png':'images/pause.png'
+    };
+    this.playImage = playImage;
+    this.containerElement = playContainer;
+    this.pause_or_play = this.pause_or_play.bind(this);
+    this.playImage.addEventListener('click', this.pause_or_play);
+    this.playAudioCallBack = playAudioCallBack;
+    this.pauseAudioCallBack = pauseAudioCallBack;
   }
-  // TODO(you): Add methods as necessary.
+
+
+  show(){
+    this.containerElement.classList.remove('inactive');
+  }
+
+  pause_or_play(event){
+    const elem = event.currentTarget;
+    const url = elem.src;
+    if(url.includes('pause')){
+      elem.src = 'images/play.png';
+      this.pauseAudioCallBack();
+
+    }else{
+      elem.src = 'images/pause.png';
+      this.playAudioCallBack();
+
+    }
+  }
 }
