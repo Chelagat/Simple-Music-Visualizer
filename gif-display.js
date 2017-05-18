39fcc399a034c43bf0473ab2e47cf6752dc28e94
Norah Borus <nborus@stanford.edu>
2017-05-17 18:29:13 -0700

    Update gif-display.js

--- gif-display.js
+++ gif-display.js
@@ -1,10 +1,28 @@
 // This class will represent the gif display area. It keeps track of which gif
 // is being shown and can select a new random gif to be shown.
-// 
+//
 // See HW4 writeup for more hints and details.
 class GifDisplay {
-  constructor() {
-    // TODO(you): Implement the constructor and add fields as necessary.
+  constructor(containerElement, urls, startIndex) {
+    this.containerElement = containerElement;
+    this.playNextGif = this.playNextGif.bind(this);
+    this.currIndex =  startIndex;
+    this.length  = urls.length;
+    this.containerElement.style.backgroundImage = "url('"+urls[this.currIndex]+"')";
+    this.containerElement.classList.add('giphy-background');
+    this.urls = urls;
+
+
+
+
   }
-  // TODO(you): Add methods as necessary.
+
+  playNextGif(otherIndex){
+    console.log("PlayNexTGif");
+    this.currIndex++;
+    if(this.currIndex % this.length === otherIndex) this.currIndex++;
+    this.containerElement.style.backgroundImage = "url('"+this.urls[this.currIndex % this.length]+"')";
+
+  }
+
 }
